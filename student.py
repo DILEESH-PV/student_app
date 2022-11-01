21bbf85d0ac3a4fd7220df6c48b0b1f5dc8dd255
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-01 14:48:50 +0530

    first command

---
+++ student.py
@@ -0,0 +1,22 @@
+while True:
+    print("Select an option")
+    print("1 add student")
+    print("2 view all student")
+    print("3 search a student")
+    print("4 update the student")
+    print("5 delete a student")
+    print("6 exit")
+
+    ch=int(input("select an option"))
+    if(ch==1):
+        print("selected add student")
+    elif(ch==2):
+        print("selected view all student")
+    elif(ch==3):
+        print("selected search student")
+    elif(ch==4):
+        print("selected update the student")
+    elif(ch==5):
+         print("selected delete student")
+    elif(ch==6):
+        break
